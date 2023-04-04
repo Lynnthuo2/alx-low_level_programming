2d0aefb0d2c6467a5e915d990262a3082da7fdcd
lynnthuo2 <lynnnjeri890@gmail.com>
2023-04-04 16:23:17 -0700

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,14 +8,11 @@
 */
 char *_strchr(char *s, char c)
 {
-	int b = 0
+	int b = 0;
 
-	{
-		if (s[b] == c)
-			return (s)
-			{
-		else
-			return (0)
-      }
+	for  (; s[b] >= '\0'; b++)
+	{if (s[b] == c)
+	  return (&s[b]);
   }
+	return (0);
 }
